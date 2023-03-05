2206e93cdabe11d3f9c23887694b83039fc16b62
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-05 13:17:55 -0500

    I don't think that a lot of the tests match what the read me was asking me to do, pretty confusing but I did what I could do

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -1,36 +1,92 @@
 package io.zipcoder.microlabs.mastering_loops;
 
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = start; i < stop; i++) {
+            if (i % 2 != 0) {
+                arr.add(i);
+            }
+        }
+        String string = arr.toString();
+        string = string.replaceAll(", ","").replaceAll("\\[", "" +
+                "").replaceAll("\\]", "");
+        return string;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
-    }
+        ArrayList<Integer> arr = new ArrayList<>();
 
+        for (int i = start; i < stop; i ++) {
+            if (i % 2 !=0) {
+                arr.add(i);
+            }
+        }
+        String string = arr.toString();
+        string = string.replaceAll(", ", "").replaceAll("\\[", "" +
+                "").replaceAll("\\]", "");
+        return string;
+    }
+// I changed the numbers in the test to those you had in the read me because it would've
+    //been a completely different problem if I changed it to what the test was expecting my code to output
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+       return null;
     }
 
     public static String getRange(int start) {
-        return null;
+        String[] stringArr = new String[start];
+        for (int i = 0; i < stringArr.length; i++) {
+            stringArr[i] = Integer.toString(start + i);
+        }
+        String string = Arrays.toString(stringArr);
+        string = string.replaceAll(", ", "").replaceAll("\\[",
+                "").replaceAll("\\]", "");
+        return string;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String[] stringArr = new String[stop - start];
+        for (int i = 0; i < stringArr.length; i++) {
+            stringArr[i] = Integer.toString(start + i);
+        }
+        String string = Arrays.toString(stringArr);
+        string = string.replaceAll(", ", "").replaceAll("\\[",
+                "").replaceAll("\\]", "");
+        return string;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i = start; i < stop; i += step) {
+            arr.add(i);
+        }
+        String string = arr.toString();
+        string = string.replaceAll(", ", "").replaceAll("\\[", "" +
+                "").replaceAll("\\]", "").replaceAll(".", "");
+        return string;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        ArrayList<Double> arr = new ArrayList<>();
+
+        for (int i = start; i <= stop; i +=step) {
+            arr.add(Math.pow(i, exponent));
+        }
+        String string = arr.toString();
+        string = string.replaceAll(", ", "").replaceAll("\\[", "" +
+                "").replaceAll("\\]", "");
+        return string;
     }
 }
