e55a0bc070cb1568aafd1ed52b5bab273899d9fd
Jennifer <jennifer@Zipcoders-MacBook-Pro-9.local>
2023-03-05 11:43:44 -0500

    Finished the table utilities, about to complete number utl

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,42 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= 5; j++) {
+                String small = String.format("%3s |", i * j);
+                sb.append(small);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++) {
+                String large = String.format("%3s |", i * j);
+                sb.append(large);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
+
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 20; i++)
+        {
+            for (int j = 1; j <= 20; j++) {
+                String table = String.format("%3s |", i * j);
+                sb.append(table);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
-}
+    }
